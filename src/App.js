6a5699e9461aa2210222ee1bc93908b006bfbf31
sashaPlavi <albingulac@gmail.com>
2021-12-05 21:53:08 +0100

    global style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
  import {Container} from './components/styles/Container.styled'
  import Header from './components/Header';
  import {Theme} from './components/styles/Theme.stiled'
+ import GlobalStyles from './components/styles/Global.styled';
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
 
     <>
+    <GlobalStyles/>
     <Header/>
     <Container>
     dankica car
